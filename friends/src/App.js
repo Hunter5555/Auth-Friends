e4bb406254d6cd660995ea55e35e839d7ca61603
Hunter5555 <hunter.m.allan@gmail.com>
2020-01-28 15:41:26 -0700

    mvp

---
+++ friends/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Routes from './utils/Routes';
+import Header from "./Header";
+import { Container } from 'semantic-ui-react';
+import "semantic-ui-css/semantic.min.css";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="App">
+          <Routes />
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ friends/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Router } from 'react-router-dom';
+import history from './utils/history';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <Router history={history}>
+      <App />
+    </Router>, document.getElementById('root')); 
